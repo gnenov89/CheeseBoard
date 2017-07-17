17d4061b7783047df1ac40e6b8ae3c6badca8289
Georgi Nenov <georginenov@Georgis-MacBook-Pro.local>
2017-07-17 18:08:18 -0400

    second draft

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -16,30 +16,40 @@ $ (document).ready(function(){
 
   var goalScore = Math.floor(Math.random() * 120) + 1;
   
-  var userWins = "";
-  
+  var userWins = 1;
+  console.log("number " + userWins);
   var userLoses = "";
   
   var currentScore = "";
 
+// Changes the Goal score section by selecting an id in the html file
   $("#putka>h1").html(goalScore);
+
+// Changes the Current score section by selcting an id in the html file 
   $("#current-score>h6").html(currentScore);
 
+// Generates a random number on click 
   $("#feta").click(function(){
   
     currentScore = currentScore + cheeseBoard[0];
     $("#current-score>h6").html(currentScore);
 });
+  // Generates a random number on click 
+
 $("#mozarella").click(function(){
 
     currentScore = currentScore + cheeseBoard[1];
     $("#current-score>h6").html(currentScore);
 });
+// Generates a random number on click 
+
 $("#gorgonzola").click(function(){
 
     currentScore = currentScore + cheeseBoard[2];
     $("#current-score>h6").html(currentScore);
 });
+// Generates a random number on click 
+
 $("#talegio").click(function(){
 
     currentScore = currentScore + cheeseBoard[3];
@@ -49,9 +59,13 @@ $("#talegio").click(function(){
 
 });
 
+// this will/should add to the Wins section
+
 if (currentScore = goalScore) {
 
     $("#wins").text(userWins++);
+
+ // this will add to the Losses section
 
 } else if (currentScore < goalScore){
 
